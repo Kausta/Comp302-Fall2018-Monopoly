3068d2a7130fc0e3deb692e2eb398afd3a3184d5
M.Besher Massri <mmassri17@ku.edu.tr>
2018-11-09 17:09:32 +0300

    Updated Game class

--- src/cabernet1/monopoly/domain/Game.java
+++ src/cabernet1/monopoly/domain/Game.java
@@ -1,13 +1,31 @@
 package cabernet1.monopoly.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import cabernet1.monopoly.domain.board.Board;
+import cabernet1.monopoly.domain.die.util.NormalDiceCup;
+import cabernet1.monopoly.domain.die.util.RollThreeDiceCup;
+import cabernet1.monopoly.domain.player.Player;
+
 public class Game {
     private static volatile Game _instance = null;
     private GameController controller;
-
+    
+    private Board gameBoard;
+    private List<Player> player;
+    private NormalDiceCup normalCup;
+    private RollThreeDiceCup rollThreeCup;
+    
     private Game() {
-
+    	initialization();
     }
-
+    private void initialization() {
+    	gameBoard=new Board();
+    	player=new ArrayList<>();
+    	normalCup=new NormalDiceCup();
+    	rollThreeCup=new RollThreeDiceCup();
+    }
     public static synchronized Game getInstance() {
         if (_instance == null) {
             _instance = new Game();
@@ -20,5 +38,8 @@ public class Game {
             controller = new GameController();
         }
         return controller;
+    }
+    public void endTurn() {
+    	//TODO: implement endTurn function
     }
 }
