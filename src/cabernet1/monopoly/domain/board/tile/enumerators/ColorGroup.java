b564ebd7046bf8cc8d8658c9aa117909d726597d
M.Besher Massri <mmassri17@ku.edu.tr>
2018-11-09 17:08:16 +0300

    Fixed a typo

--- src/cabernet1/monopoly/domain/board/tile/enumerators/ColorGroup.java
+++ src/cabernet1/monopoly/domain/board/tile/enumerators/ColorGroup.java
@@ -1,7 +1,7 @@
 package cabernet1.monopoly.domain.board.tile.enumerators;
 
 /**
- * The enumarator for color groups
+ * The enumerator for color groups
  */
 public enum ColorGroup { 
     Orange, 
