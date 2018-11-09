12192e1982c890e13503207813ee13bcda021e57
Caner Korkmaz <info@canerkorkmaz.com>
2018-11-09 17:13:39 +0300

    Added JUnit5

---
+++ test/cabernet1/monopoly/ExampleTest.java
@@ -0,0 +1,13 @@
+package cabernet1.monopoly;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleTest {
+    @Test
+    public void test1Plus1() {
+        int sum = 1 + 1;
+        assertEquals(2, sum);
+    }
+}
