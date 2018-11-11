15713874177767a0ec4b47a8e46a6e601fbf936c
M.Besher Massri <mmassri17@ku.edu.tr>
2018-11-11 17:40:01 +0300

    Implemented some handling-moves methods

--- src/cabernet1/monopoly/domain/bot/BotPlayer.java
+++ src/cabernet1/monopoly/domain/bot/BotPlayer.java
@@ -3,62 +3,83 @@ package cabernet1.monopoly.domain.bot;
 import cabernet1.monopoly.domain.board.Board;
 import cabernet1.monopoly.domain.board.tile.Tile;
 import cabernet1.monopoly.domain.die.IDie;
+import cabernet1.monopoly.domain.die.util.JailDiceCup;
 import cabernet1.monopoly.domain.die.util.NormalDiceCup;
 import cabernet1.monopoly.domain.player.IPlayer;
 
 public class BotPlayer extends IPlayer {
-    private IStrategy botStrategy;
+	private IStrategy botStrategy;
 
-    public BotPlayer(String name, int money, int defaultOrder,Tile currentTile, IStrategy botStrategy) {
-        super(name, money, defaultOrder,currentTile);
-        this.botStrategy = botStrategy;
-    }
+	public BotPlayer(String name, int money, int defaultOrder, Tile currentTile, IStrategy botStrategy) {
+		super(name, money, defaultOrder, currentTile);
+		this.botStrategy = botStrategy;
+	}
 
-    @Override
-    public void playTurn() {
-        botStrategy.execute();
-    }
+	@Override
+	public void playTurn() {
+		
+	}
 
-    public IStrategy getBotStrategy() {
-        return botStrategy;
-    }
+	public IStrategy getBotStrategy() {
+		return botStrategy;
+	}
 
-    public void setBotStrategy(IStrategy botStrategy) {
-        this.botStrategy = botStrategy;
-    }
+	public void setBotStrategy(IStrategy botStrategy) {
+		this.botStrategy = botStrategy;
+	}
 
 	@Override
 	public void playTurn(NormalDiceCup dice, Board board) {
-		// to be implemented later
+		botStrategy.execute();
+		//TODO implement bot-playTurn
+	}
+
+	
+	@Override
+	protected void goJail(Board board) {
+		// TODO Auto-generated method stub
+
 	}
 
 	@Override
-	protected Tile handleNormalMove() {
+	public void playJailturn(JailDiceCup cup, Board board) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected Tile handleNormalMove(NormalDiceCup cup, Board board) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	protected Tile handleMrMonopolyMove() {
+	protected Tile handleMrMonopolyMove(NormalDiceCup cup, Board board) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	protected Tile handleBusMove() {
+	protected Tile handleBusMove(NormalDiceCup cup, Board board) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	protected Tile handleTriplesMove() {
+	protected Tile handleTriplesMove(NormalDiceCup cup, Board board) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	protected Tile handleDoubleMove() {
+	protected Tile handleDoubleMove(NormalDiceCup cup, Board board) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public void jumpToTile(Tile newTile) {
+		// TODO Auto-generated method stub
+		
 	}
 }
--- src/cabernet1/monopoly/domain/player/Player.java
+++ src/cabernet1/monopoly/domain/player/Player.java
@@ -1,8 +1,13 @@
 package cabernet1.monopoly.domain.player;
 
+import java.lang.invoke.SwitchPoint;
+
 import cabernet1.monopoly.domain.board.Board;
 import cabernet1.monopoly.domain.board.tile.Tile;
+import cabernet1.monopoly.domain.die.enumerators.JailDiceCupStatus;
 import cabernet1.monopoly.domain.die.enumerators.NormalDiceCupStatus;
+import cabernet1.monopoly.domain.die.util.DiceCup;
+import cabernet1.monopoly.domain.die.util.JailDiceCup;
 import cabernet1.monopoly.domain.die.util.NormalDiceCup;
 
 public class Player extends IPlayer {
@@ -13,57 +18,101 @@ public class Player extends IPlayer {
 	@Override
 	public void playTurn(NormalDiceCup cup, Board board) {
 		NormalDiceCupStatus rollStatus = cup.rollCup();
+		//TODO announceMessage of the dice result to UI
+		//TODO changeDiceFaces(die1: DieFaces,die2: DieFaces,die3: DieFaces)
 		switch (rollStatus) {
 		case normalMove:
-			handleNormalMove();
+			handleNormalMove(cup,board);
 			break;
 		case DoubleMove:
-			handleDoubleMove();
+			handleDoubleMove(cup,board);
 			break;
 		case TripleMove:
-			handleTriplesMove();
+			handleTriplesMove(cup,board);
 			break;
 		case MrMonopolyMove:
-			handleMrMonopolyMove();
+			handleMrMonopolyMove(cup,board);
 			break;
 		case BusMove:
-			handleBusMove();
+			handleBusMove(cup,board);
 			break;
 		}
 
 	}
-
 	@Override
-	protected Tile handleNormalMove() {
-		// TODO Auto-generated method stub
+	public void playJailturn(JailDiceCup cup, Board board) {
+		JailDiceCupStatus rollStatus= cup.rollCup();
+		switch (rollStatus) {
+		case doubles:
+			//TODO announce to UI that user goes out of Jail
+			//TODO: handleDoubleMove for JailCup
+			//handleDoubleMove(cup,board);
+			break;
+		case notDoubles:
+			//TODO announce to UI that user still in jail
+			break;
+		}
+		
+	}
+	@Override
+	protected Tile handleNormalMove(NormalDiceCup cup, Board board) {
+		Tile newTile=board.getNextTile(curTile,cup.getFacesValue());
+		//TODO call the ICommand for changeCurrentTile(newTile)
+		//TODO announceMessage of transportation
+		//call interface.movePlayer
 		return null;
 	}
 
 	@Override
-	protected Tile handleMrMonopolyMove() {
+	protected Tile handleMrMonopolyMove(NormalDiceCup cup, Board board) {
 		// TODO implement handleMrMonopolyMove function
 		return null;
 	}
 
 	@Override
-	protected Tile handleBusMove() {
+	protected Tile handleBusMove(NormalDiceCup cup, Board board) {
 		// TODO implement handleBusIconMove function
 		return null;
 	}
 
 	@Override
-	protected Tile handleTriplesMove() {
+	protected Tile handleTriplesMove(NormalDiceCup cup, Board board) {
 		// TODO implement handleTriplesMove function
+		// TODO call the interface.chooseTile observer
 		return null;
+	}
+	
+	
+
+	@Override
+	protected Tile handleDoubleMove(NormalDiceCup cup, Board board) {
+		++numberOfConsecutiveDoublesRolls;
+		if (numberOfConsecutiveDoublesRolls==3) {
+			goJail(board);
+			return board.getJailTile();
+		}
+		setLastMoveDouble(true);
+		return handleNormalMove(cup,board);
 	}
 
 	@Override
-	protected Tile handleDoubleMove() {
-		// TODO handleDoubleMove function
-		++numberOfConsecutiveDoublesRolls;
-		if (numberOfConsecutiveDoublesRolls==3) {
-			numberOfConsecutiveDoublesRolls=0;
-		}
-		return null;
+	protected void goJail(Board board) {
+		getInJail();
+		numberOfConsecutiveDoublesRolls=0;
+		//TODO order the interface to moveTile
+		//TODO ask about using card to get out of jail
+		changeCurrentTile(board.getJailTile());
 	}
+
+	
+
+	@Override
+	public void jumpToTile(Tile newTile) {
+		//changeCurrentTile(newTile);
+		//TODO call the ICommand for changeCurrentTile(newTile)
+		//TODO announceMessage of transportation
+		
+	}
+
+
 }
