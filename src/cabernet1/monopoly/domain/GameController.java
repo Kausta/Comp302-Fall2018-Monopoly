d60a56c0a8704bb326d9e6d139cd5352e13f0409
Cüneyt Emre Yavuz <cyavuz15@ku.edu.tr>
2018-11-17 23:11:27 +0300

    Observer pattern is implemented for LogPanel

--- src/cabernet1/monopoly/domain/GameController.java
+++ src/cabernet1/monopoly/domain/GameController.java
@@ -5,12 +5,17 @@ package cabernet1.monopoly.domain;
 
 import cabernet1.monopoly.logging.Logger;
 import cabernet1.monopoly.logging.LoggerFactory;
+import cabernet1.monopoly.utils.Observable;
 
 public class GameController {
     private Logger logger = LoggerFactory.getInstance().getLogger(getClass());
+
+    // To add announcements to UI
+    public Observable<String> announcement = new Observable<>();
 
     public GameController() {
         logger.i("Created Game Controller");
 
     }
+
 }
