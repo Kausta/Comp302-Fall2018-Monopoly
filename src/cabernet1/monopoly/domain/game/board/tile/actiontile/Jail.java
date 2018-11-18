6516dc0f03d6e5556dd9201f38435dc3944e06c4
Alper Toygar <alpertoygar@yandex.com>
2018-11-18 15:07:25 +0300

    implemented buy property and added jail

---
+++ src/cabernet1/monopoly/domain/game/board/tile/actiontile/Jail.java
@@ -0,0 +1,21 @@
+package cabernet1.monopoly.domain.game.board.tile.actiontile;
+
+import cabernet1.monopoly.domain.game.board.tile.enumerators.TileType;
+
+public class Jail extends ActionTile {
+
+    public Jail(){
+        super("Jail", TileType.Jail);
+    }
+
+    @Override
+    public void landingAction() {
+        // TODO: implement
+    }
+
+    @Override
+    public void passingAction() {
+
+    }
+
+}
