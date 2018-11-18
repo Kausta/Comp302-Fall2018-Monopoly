ece5f8ebff5aca9850de01d8b578060a117405a4
Caner Korkmaz <info@canerkorkmaz.com>
2018-11-18 16:49:36 +0300

    Added game initialization

--- src/cabernet1/monopoly/Application.java
+++ src/cabernet1/monopoly/Application.java
@@ -12,29 +12,52 @@ import javax.swing.*;
 import java.io.IOException;
 
 public class Application implements Runnable {
+    private static Application _instance = null;
     private Logger logger = LoggerFactory.getInstance().getLogger(getClass());
 
-    public Application() {
+    private Application() {
         logger.i("Created new Monopoly Instance");
     }
 
+    public static synchronized Application getInstance() {
+        if (_instance == null) {
+            _instance = new Application();
+        }
+        return _instance;
+    }
+
     public static void main(String[] args) {
-        Application application = new Application();
+        Application application = Application.getInstance();
         SwingUtilities.invokeLater(application);
     }
 
     @Override
     public void run() {
         logger.i("Running Monopoly");
+        startInitialization();
+    }
 
-        // TODO: Make initialization happen after the user picks server or client mode in the UI
+    public void startInitialization() {
+
+    }
+
+    public void startClient(String ip, int port) {
         try {
-            Network.getInstance().initializeServer(11000);
+            Network.getInstance().initializeClient(ip, port);
         } catch (IOException e) {
             logger.e("Cannot start program");
-            return;
         }
+    }
 
+    public void startServer(int port) {
+        try {
+            Network.getInstance().initializeServer(port);
+        } catch (IOException e) {
+            logger.e("Cannot start program");
+        }
+    }
+
+    public void startGame() {
         Game game = Game.getInstance();
         GameController controller = game.getGameController();
         GameView view = GameView.getInstance();
@@ -42,3 +65,4 @@ public class Application implements Runnable {
         ViewUtils.createWindowFromView(view);
     }
 }
+
