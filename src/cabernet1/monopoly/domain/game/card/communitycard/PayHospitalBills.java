d2180def0021a907dc5ccf8a880e64d8ab618091
M.Besher Massri <mmassri17@ku.edu.tr>
2018-11-18 23:23:27 +0300

    Added Board-related commands

--- src/cabernet1/monopoly/domain/game/card/communitycard/PayHospitalBills.java
+++ src/cabernet1/monopoly/domain/game/card/communitycard/PayHospitalBills.java
@@ -1,9 +1,14 @@
 package cabernet1.monopoly.domain.game.card.communitycard;
 
+import cabernet1.monopoly.domain.Network;
+import cabernet1.monopoly.domain.NetworkController;
 import cabernet1.monopoly.domain.game.board.Board;
 import cabernet1.monopoly.domain.game.board.Pool;
 import cabernet1.monopoly.domain.game.card.IimmediateAction;
 import cabernet1.monopoly.domain.game.player.IPlayer;
+import cabernet1.monopoly.domain.network.command.commands.AnnounceMessageCommand;
+import cabernet1.monopoly.domain.network.command.commands.PayRentCommand;
+import cabernet1.monopoly.domain.network.command.commands.PoolIncreaseCommand;
 
 public class PayHospitalBills extends CommunityChestCard implements IimmediateAction {
 
@@ -12,10 +17,14 @@ public class PayHospitalBills extends CommunityChestCard implements IimmediateAc
     }
     @Override
     public void action(IPlayer player) {
-        Board board = Board.getInstance();
-        Pool pool = board.getPoolTile();
-        pool.addMoney(100);
-        player.loseMoney(100);
+    	NetworkController nc = Network.getInstance().getNetworkController();
+        
+        nc.sendCommand(new PoolIncreaseCommand(100));
+        nc.sendCommand(new PayRentCommand(player, 100));
+        
+        String message=player.getName()+" has to pay 100 for the hospital bills";
+        nc.sendCommand(new AnnounceMessageCommand(message));
+
     }
 
 }
