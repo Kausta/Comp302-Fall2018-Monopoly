b36a79b3075d7df69a0b9b02e6b2110b1d174c57
Caner Korkmaz <info@canerkorkmaz.com>
2018-12-08 20:13:12 +0300

    Configured tests and added a sample test

---
+++ src/test/java/cabernet1/monopoly/tests/SampleTest.java
@@ -0,0 +1,12 @@
+package cabernet1.monopoly.tests;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SampleTest {
+
+    @Test
+    public void twoPlusTwoIsFour() {
+        assertEquals(4, 2+2);
+    }
+}
