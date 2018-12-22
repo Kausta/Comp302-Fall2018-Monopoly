266110f0d82cbf43b953955d15877316a0fa3ded
Caner Korkmaz <info@canerkorkmaz.com>
2018-12-22 20:27:39 +0300

    Fixed a typo

--- src/main/java/cabernet1/monopoly/lib/persistence/Saveable.java
+++ src/main/java/cabernet1/monopoly/lib/persistence/Saveable.java
@@ -3,7 +3,7 @@ package cabernet1.monopoly.lib.persistence;
 import java.lang.annotation.*;
 
 /**
- * Create a marker annonation for saveable classes, which we will use on runtime to detect whether a class is saveable
+ * Create a marker annotation for saveable classes, which we will use to check whether that class is actually saveable
  * <br />
  * Note that a saveable class must be a singleton with a private static variable of its same type and an empty private constructor
  */
