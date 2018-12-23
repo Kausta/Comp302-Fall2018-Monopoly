5641244f0dade7de55c0a963fe3ca9ac2ca47a42
Caner Korkmaz <info@canerkorkmaz.com>
2018-12-23 17:00:59 +0300

    Made deserialization test more controlled

--- src/test/java/cabernet1/monopoly/lib/persistance/GameSerializerTests.java
+++ src/test/java/cabernet1/monopoly/lib/persistance/GameSerializerTests.java
@@ -2,11 +2,14 @@ package cabernet1.monopoly.lib.persistance;
 
 import cabernet1.monopoly.TestBase;
 import cabernet1.monopoly.domain.Game;
+import cabernet1.monopoly.domain.game.bot.BotPlayer;
 import cabernet1.monopoly.domain.game.bot.BotStrategyFactory;
 import cabernet1.monopoly.domain.game.die.cup.JailDiceCup;
 import cabernet1.monopoly.domain.game.die.cup.NormalDiceCup;
 import cabernet1.monopoly.domain.game.die.cup.RollThreeDiceCup;
+import cabernet1.monopoly.domain.game.player.IPlayer;
 import cabernet1.monopoly.domain.game.player.InitialPlayerData;
+import cabernet1.monopoly.lib.persistence.GameSaverRegistry;
 import cabernet1.monopoly.lib.persistence.GameSerializer;
 import org.junit.jupiter.api.Test;
 
@@ -14,8 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GameSerializerTests extends TestBase {
 
@@ -42,7 +44,8 @@ public class GameSerializerTests extends TestBase {
     public void gameSerializesAndDeserializesCorrectly() {
         // Create all instances
         List<InitialPlayerData> initialPlayerData = new ArrayList<>();
-        initialPlayerData.add(new InitialPlayerData(0, "Test Name", "127.0.0.1:1000", false));
+        InitialPlayerData playerData = new InitialPlayerData(0, "Test Name", "127.0.0.1:1000", false);
+        initialPlayerData.add(playerData);
         Game.getInstance().initialize(initialPlayerData);
         RollThreeDiceCup.getInstance();
         NormalDiceCup.getInstance();
@@ -54,8 +57,16 @@ public class GameSerializerTests extends TestBase {
             assertNotNull(serialized);
             System.out.println("Serialized into: ");
             System.out.println(serialized);
+            // Set game to null so that we will know whether it deserialized successfully
+            GameSaverRegistry.getInstance().setClassInstance("game", null);
             GameSerializer.getInstance().deserializeGameAndLoad(serialized);
             System.out.print("Deserialized successfully");
+            List<IPlayer> players = Game.getInstance().getPlayers();
+            assertEquals(1, players.size());
+            IPlayer firstPlayer = players.get(0);
+            assertEquals(playerData.getId(), firstPlayer.getID());
+            assertEquals(playerData.getName(), firstPlayer.getName());
+            assertEquals(playerData.isBotPlayer(), firstPlayer instanceof BotPlayer);
         });
     }
 
